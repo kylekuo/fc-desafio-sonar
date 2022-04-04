8dd7107a2e3d3739fa6a9edc61d5a5a70fa99c5b
Kyle Kuo <kyle.tt.kuo@gmail.com>
2022-04-04 16:58:55 -0300

    test: :pencil2: adds one more check to test

--- src/index.test.js
+++ src/index.test.js
@@ -3,4 +3,5 @@ const isEven = require('./index')
 test('checks isEven function', () => {
   expect( isEven(1) ).toBe( false );
   expect( isEven(2) ).toBe( true );
+  expect( isEven(3) ).toBe( false );
 })
